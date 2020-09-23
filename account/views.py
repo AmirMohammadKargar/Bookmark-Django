b7238d874b3bc2be44d12f6e01e2bd4b167ccd24
AM-Kargar <am-kargar@localhost.localdomain>
2020-09-23 14:21:46 +0330

    Edit wrong syntax for making form in view file in account app.

--- account/views.py
+++ account/views.py
@@ -20,6 +20,6 @@ def user_login(request):
                     return HttpResponse('Disabled account')
             else:
                 return HttpResponse('Invalid login')
-        else:
-            form = LoginForm()
+    else:
+        form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
