f92b38ec639856be62d4790de152dd7be7f95054
AM-Kargar <am-kargar@localhost.localdomain>
2020-09-24 10:02:05 +0330

    Add media url and directory to settings file.

--- SocialWeb/settings.py
+++ SocialWeb/settings.py
@@ -108,6 +108,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
 LOGIN_URL = 'login'
