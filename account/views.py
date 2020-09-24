014dbf5ddf3fd89a7ef5d05d2ae812213489336d
AM-Kargar <am-kargar@localhost.localdomain>
2020-09-24 11:33:19 +0330

    Add to edit function view in account app view.

--- account/views.py
+++ account/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login
 from .form import LoginForm,UserRegistrationForm,UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+from django.contrib import messages
 
 def dashboard(request):
     return render(request,
@@ -34,6 +35,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated ' 'successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
