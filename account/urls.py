2f20eeadd5028d8333818585f5aca8b936a53354
AM-Kargar <am-kargar@localhost.localdomain>
2020-09-24 09:46:29 +0330

    Add register url to account app url.

--- account/urls.py
+++ account/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('password_reset/',auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html'),name='password_reset'),
     path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='registration/password_resetdone.html'),name='password_reset_done'),
     path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_resetconfirm.html'),name='password_reset_confirm'),
-    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_resetcomplete.html'),name='password_reset_complete'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_resetcomplete.html'), name='password_reset_complete'),
+    path('register/', views.register, name='register'),
 ]
