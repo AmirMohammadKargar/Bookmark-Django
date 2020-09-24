34922b7eb1e48016f6a9a8826772a33d20d3b0a5
AM-Kargar <am-kargar@localhost.localdomain>
2020-09-24 10:10:22 +0330

    Add edit function to view of account app for user edit profile.

--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .form import LoginForm,UserRegistrationForm
+from .form import LoginForm,UserRegistrationForm,UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
 
@@ -24,4 +24,17 @@ def register(request):
             return render(request,'account/register_done.html',{'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
-    return render(request,'account/register.html',{'user_form': user_form})
+    return render(request, 'account/register.html', {'user_form': user_form})
+    
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile,data=request.POST,files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(request,'account/edit.html',{'user_form': user_form,'profile_form': profile_form})
