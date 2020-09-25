3bbff02004114433d495d9563445bfb825064680
AM-Kargar <am-kargar@localhost.localdomain>
2020-09-25 11:44:10 +0330

    Edit bookmarklet javascript file for user can save image in data base.

--- image/static/js/bookmarklet.js
+++ image/static/js/bookmarklet.js
@@ -30,6 +30,18 @@
                     image_url + '" /></a>');
             }
         });
+        // when an image is selected open URL with it
+        jQuery('#bookmarklet .images a').click(function (e) {
+            selected_image = jQuery(this).children('img').attr('src');
+            // hide bookmarklet
+            jQuery('#bookmarklet').hide();
+            // open new window to submit the image
+            window.open(site_url + 'images/create/?url='
+                + encodeURIComponent(selected_image)
+                + '&title='
+                + encodeURIComponent(jQuery('title').text()),
+                '_blank');
+        });
     };
     if (typeof window.jQuery != 'undefined') {
         bookmarklet();
