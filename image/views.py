9b2528f1decfb35a5872e0e05a3dd26f704ff5d1
AM-Kargar <am-kargar@localhost.localdomain>
2020-09-25 10:53:27 +0330

    Fix template address in view.

--- image/views.py
+++ image/views.py
@@ -21,4 +21,4 @@ def image_create(request):
     else:
         # build form with data provided by the bookmarklet via GET
         form = ImageCreateForm(data=request.GET)
-    return render(request,'images/image/create.html',{'section': 'images','form': form})
+    return render(request,'image/image/create.html',{'section': 'images','form': form})
