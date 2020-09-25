0dcee7ce16b6d96086d1d99bf401c10c72007e13
AM-Kargar <am-kargar@localhost.localdomain>
2020-09-25 10:11:29 +0330

    Edit main url and add image app url to it.

--- SocialWeb/urls.py
+++ SocialWeb/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
-    path('social-auth/',include('social_django.urls', namespace='social')),
+    path('social-auth/', include('social_django.urls', namespace='social')),
+    path('images/', include('image.urls', namespace='image')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
