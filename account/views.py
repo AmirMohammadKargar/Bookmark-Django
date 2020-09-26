ff5f871ea7789b244c6834e8a0f9961e22e64713
AM-Kargar <am-kargar@localhost.localdomain>
2020-09-26 15:16:51 +0330

    Edit account app view file and user list and detail function to it.

--- account/views.py
+++ account/views.py
@@ -5,6 +5,8 @@ from .form import LoginForm,UserRegistrationForm,UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
 from django.contrib import messages
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
 
 def dashboard(request):
     return render(request,'account/dashboard.html',{'section': 'dashboard'})
@@ -39,4 +41,14 @@ def edit(request):
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
-    return render(request,'account/edit.html',{'user_form': user_form,'profile_form': profile_form})
+    return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})
+    
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active=True)
+    return render(request, 'account/user/list.html', {'section': 'people', 'users': users})
+    
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User,username=username,is_active=True)
+    return render(request,'account/user/detail.html',{'section': 'people','user': user})
